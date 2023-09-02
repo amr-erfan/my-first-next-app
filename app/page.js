b616fb6a9ecf3754d269ce05009a06287b601e09
unknown <amr_fouad59@yahoo.com>
2023-09-03 00:10:57 +0200

    first update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Fragment } from "react";
+import React from "react";
+import Link from "next/link";
+export const metadata = {
+  title: "post page",
+};
 
-export default function Home() {
-  return <Fragment></Fragment>;
-}
+const page = async () => {
+  const data = await fetch(" http://jsonplaceholder.org/posts", {
+    next: {
+      revalidate: 60, //ISG incremental static generation
+    },
+  });
+  const result = await data.json();
+  return (
+    <div
+      className="home-post-div"
+      style={{ width: "100%", backgroundColor: "black" }}
+    >
+      posts page
+      <Link href="/articles">article page</Link>
+      {result.map((res) => {
+        return (
+          <Link href={`/posts/${res.id}`}>
+            <div
+              key={res.id}
+              className="post-div"
+              style={{
+                width: "60%",
+                padding: "10px",
+                margin: "20px auto",
+                color: "black",
+                backgroundColor: "white",
+                borderRadius: "20px",
+              }}
+            >
+              <h1> title: {res.title}</h1>
+              <h1> desc: {res.content}</h1>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default page;
